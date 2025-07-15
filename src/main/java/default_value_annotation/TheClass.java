f474fe875adf0b41a4b9071b5992359bfd1e1479
OleksandrObydalo <oleksandr.obydalo@nure.ua>
2025-07-15 21:25:10 +0300

    Add example of reading annotation parameter from method

--- src/main/java/default_value_annotation/TheClass.java
+++ src/main/java/default_value_annotation/TheClass.java
@@ -1,5 +1,20 @@
 package default_value_annotation;
 
+import java.lang.reflect.Method;
+
 @MyAnnotation(name = "someName")
 public class TheClass {
+    @MyAnnotation(name = "someName", value = "From Method")
+    public void doSomething() { }
+
+    public static void main(String[] args) {
+        try {
+            Method method = TheClass.class.getMethod("doSomething");
+            MyAnnotation annotation = method.getAnnotation(MyAnnotation.class);
+            System.out.println("name: " + annotation.name());
+            System.out.println("value: " + annotation.value());
+        } catch (NoSuchMethodException e) {
+            // Handle exception
+        }
+    }
 }
