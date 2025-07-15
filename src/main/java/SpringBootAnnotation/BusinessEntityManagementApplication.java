7ae1da82bf8cbcabb62dc4ceef06832c3f67744c
OleksandrObydalo <oleksandr.obydalo@nure.ua>
2025-07-15 20:20:48 +0300

    Add example with SpringBoot anontation

---
+++ src/main/java/SpringBootAnnotation/BusinessEntityManagementApplication.java
@@ -0,0 +1,11 @@
+package SpringBootAnnotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class BusinessEntityManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessEntityManagementApplication.class, args);
+    }
+}
